dca53d2058a2ec0c8c1a7e768623e85a4fcd8393
Ajay0630 <78241226+Ajay0630@users.noreply.github.com>
2021-02-13 21:20:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function link57() {
+    window.alert("Please copy and paste the link in a new tab: https://ajay0630.github.io/Project-57/");
+}
+function link56() {
+    window.alert("Please copy and paste the link in a new tab: https://ajay0630.github.io/Project_56/");
+}
+function link55() {
+    window.alert("Please copy and paste the link in a new tab: https://codepen.io/Ajay0630/full/YzGoJaN?editors=1010");
+}
+function link54() {
+    window.alert("Please copy and paste the link in a new tab: https://codepen.io/Ajay0630/full/LYRMWej");
+}
+function link53() {
+    window.alert("Please copy and paste the link in a new tab: https://codepen.io/Ajay0630/full/RwGJzEd");
+}
+function link52() {
+    window.alert("Please copy and paste the link in a new tab: https://codepen.io/Ajay0630/full/eYdVjvr");
+}
+function link51() {
+    window.alert("Please copy and paste the link in a new tab: https://x.thunkable.com/projects/5fe4b4def2686d0012d80cc6/c5a8adb1-c73f-4945-a023-ab7fe380747b/designer");
+}
+function link50() {
+    window.alert("Please copy and paste the link in a new tab: https://codepen.io/Ajay0630/full/NWRazKr");
+}
